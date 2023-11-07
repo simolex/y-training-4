e251034a6b819d8f5cc2cdbb9d01d02349fcb9a2
simolex <simolex@yandex.ru>
2023-11-07 23:33:08 +0300

    done task D from stage 1

--- Stage-1/task-D/index.js
+++ Stage-1/task-D/index.js
@@ -13,26 +13,26 @@
  * Выведите результат сортировки, то есть N целых чисел, разделенных пробелами.
  */
 
-function merge(n, a, m, b, buffer, l, r) {
-    let PointerA = 0;
-    let PointerB = 0;
+function merge(l1, r1, l2, r2, a, l0, b) {
+    let PointerA = l1;
+    let PointerB = l2;
 
-    const c = Array(n + m);
-    for (let i = 0; i < m + n; i++) {
-        if (PointerA < n && PointerB === m) {
+    const c = b;
+    for (let i = l0; i < l0 + r1 + r2 - l1 - l2; i++) {
+        if (PointerA < r1 && PointerB === r2) {
             c[i] = a[PointerA];
             PointerA++;
         }
-        if (PointerA === n && PointerB < m) {
-            c[i] = b[PointerB];
+        if (PointerA === r1 && PointerB < r2) {
+            c[i] = a[PointerB];
             PointerB++;
         }
-        if (PointerA < n && PointerB < m) {
-            if (a[PointerA] <= b[PointerB]) {
+        if (PointerA < r1 && PointerB < r2) {
+            if (a[PointerA] <= a[PointerB]) {
                 c[i] = a[PointerA];
                 PointerA++;
             } else {
-                c[i] = b[PointerB];
+                c[i] = a[PointerB];
                 PointerB++;
             }
         }
@@ -40,49 +40,20 @@ function merge(n, a, m, b, buffer, l, r) {
     return c;
 }
 
-function partition(a, l, r, x) {
-    let equalPointer = l;
-    let greatePointer = l;
-    let newValue;
-    for (let i = l; i < r; i++) {
-        newValue = a[i];
-        switch (true) {
-            case a[i] < x:
-                if (i !== greatePointer) {
-                    a[i] = a[greatePointer];
-                }
-                if (greatePointer !== equalPointer) {
-                    a[greatePointer] = a[equalPointer];
-                }
-                if (equalPointer !== i) {
-                    a[equalPointer] = newValue;
-                }
-                greatePointer++;
-                equalPointer++;
-                break;
-            case a[i] === x:
-                if (i !== greatePointer) {
-                    a[i] = a[greatePointer];
-                    a[greatePointer] = newValue;
-                }
-                greatePointer++;
-                break;
-            // case a[i] > x:
-            // break;
-        }
-    }
-    if (l < equalPointer) {
-        partition(a, l, equalPointer, a[l + Math.floor(Math.random() * (equalPointer - l - 1))]);
-    }
-
-    if (greatePointer + 1 < r) {
-        partition(a, greatePointer, r, a[greatePointer + Math.floor(Math.random() * (r - greatePointer - 1))]);
-    }
-    return a;
+function sortWrapper(l, r, index, buffer) {
+    const m = l + Math.floor((r - l) / 2);
+    const nextIndex = (index + 1) % 2;
+    if (m - l > 1) sortWrapper(l, m, nextIndex, buffer);
+    if (r - m > 1) sortWrapper(m, r, nextIndex, buffer);
+    return merge(l, m, m, r, buffer[nextIndex], l, buffer[index]);
 }
 
 function sort(n, a) {
-    return partition(a, 0, n, a[Math.floor(Math.random() * n)]);
+    const b = [];
+    for (let i = 0; i < 2; i++) {
+        b[i] = [].concat(a);
+    }
+    return sortWrapper(0, n, 0, b);
 }
 
 const _readline = require("readline");
