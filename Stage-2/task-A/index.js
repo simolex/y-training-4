60fe7d8d291436cbd8a74039b25e157f07226b20
simolex <simolex@yandex.ru>
2023-11-09 00:49:20 +0300

    task A - need additions hash-test

---
+++ Stage-2/task-A/index.js
@@ -0,0 +1,90 @@
+/**
+ * A. Равенство подстрок
+ *
+ * Дана строка S, состоящая из строчных латинских букв.
+ * Определите, совпадают ли строки одинаковой длины L, начинающиеся с позиций A и B.
+ *
+ * Формат ввода:
+ * В первой строке записана S (1 ≤ |S| ≤ 2 ⋅ 105), состоящая из строчных латинских букв.
+ * Во второй строке записано число Q (1 ≤ Q ≤ 2 ⋅ 105) — количество запросов.
+ * В следющих Q строках записаны запросы: целые числа L, A и B
+ * (1 ≤ L ≤ |S|, 0 ≤ A, B ≤ (|S| - L)) — длина подстрок и позиции, с которых они начинаются.
+ *
+ * Формат вывода:
+ * Если строки совпадают — выведите "yes", иначе — "no".
+ */
+
+function equalSubstrings(s, n, queries) {
+    const len = s.length;
+    const h = [0];
+    const x = [1];
+
+    const xValue = 257; //263           //269       //271       //277
+    const pMod = 1000003; //1000033     //1000037   //1000039   //1000081
+    //TODO need multiTest
+
+    const isEqual = (from_1, from_2, subLen) => {
+        return (
+            (h[from_1 + subLen] + h[from_2] * x[subLen]) % pMod === (h[from_2 + subLen] + h[from_1] * x[subLen]) % pMod
+        );
+    };
+
+    s = " " + s;
+    const sArr = s.split("");
+    for (let i = 1; i <= len; i++) {
+        h[i] = (h[i - 1] * xValue + sArr[i].charCodeAt(0)) % pMod;
+        x[i] = (x[i - 1] * xValue) % pMod;
+    }
+
+    return queries.map((q) => (isEqual(q[1], q[2], q[0]) ? "yes" : "no"));
+}
+
+const _readline = require("readline");
+
+const _reader = _readline.createInterface({
+    input: process.stdin
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on("line", (line) => {
+    _inputLines.push(line);
+});
+
+process.stdin.on("end", solve);
+
+function solve() {
+    const s = readString();
+    const n = readInt();
+    const queries = [];
+    for (let i = 0; i < n; i++) {
+        queries.push(readArray());
+    }
+
+    const result = equalSubstrings(s, n, queries);
+    console.log(result.join("\n"));
+}
+
+function readInt() {
+    const n = Number(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+
+function readString() {
+    const s = _inputLines[_curLine].trim();
+    _curLine++;
+    return s;
+}
+
+function readArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .map((num) => Number(num));
+    _curLine++;
+    return arr;
+}
+
+module.exports = equalSubstrings;
---
+++ Stage-2/task-A/index.test.js
@@ -0,0 +1,27 @@
+const solution = require(".");
+
+describe("E. Поразрядная сортировка", () => {
+    test("test-1", () => {
+        const result = solution("acabaca", 3, [
+            [4, 3, 2],
+            [3, 4, 0],
+            [2, 0, 1]
+        ]);
+        expect(result).toEqual(["no", "yes", "no"]);
+    });
+    test("test-2", () => {
+        const result = solution("caeabaeadedcbdcdccec", 10, [
+            [13, 4, 3],
+            [2, 12, 15],
+            [10, 1, 3],
+            [3, 8, 15],
+            [13, 5, 6],
+            [7, 2, 6],
+            [9, 8, 8],
+            [19, 0, 0],
+            [19, 0, 0],
+            [6, 7, 13]
+        ]);
+        expect(result).toEqual(["no", "no", "no", "no", "no", "no", "yes", "yes", "yes", "no"]);
+    });
+});
