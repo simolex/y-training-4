37accc33ae092b60edff592795b4f5f04c3eef54
simolex <simolex@yandex.ru>
2023-11-11 22:29:35 +0300

    done task D from stage 2

--- Stage-2/task-D/index.js
+++ Stage-2/task-D/index.js
@@ -22,9 +22,10 @@ const hashSettings = [
     { xValue: 269, pMod: 10 ** 7 + 103 }
 ];
 
-function lengthBasedString(s) {
-    const len = s.length;
+function mirroredString(len, c, sArr) {
+    // const len = s.length;
     const h = [];
+    const r = [];
     const x = [];
     const xValue = [];
     const pMod = [];
@@ -33,6 +34,7 @@ function lengthBasedString(s) {
 
     for (let i = 0; i < countSettings; i++) {
         h[i] = [0];
+        r[i] = [0];
         x[i] = [1];
         xValue[i] = hashSettings[i].xValue;
         pMod[i] = hashSettings[i].pMod;
@@ -44,43 +46,33 @@ function lengthBasedString(s) {
         do {
             result =
                 result &&
-                (h[j][from_1 + subLen] + h[j][from_2] * x[j][subLen]) % pMod[j] ===
-                    (h[j][from_2 + subLen] + h[j][from_1] * x[j][subLen]) % pMod[j];
+                (h[j][from_1 + subLen] + r[j][from_2] * x[j][subLen]) % pMod[j] ===
+                    (r[j][from_2 + subLen] + h[j][from_1] * x[j][subLen]) % pMod[j];
             j++;
         } while (result && j < countSettings);
         return result;
     };
 
-    const maxEqualString = (from) => {
-        let l = 0;
-        let r = len - from + 1;
-        let m;
-        while (l < r) {
-            m = Math.ceil((l + r) / 2);
-            if (isEqual(0, from, m)) {
-                l = m;
-            } else {
-                r = m - 1;
-            }
-        }
-        return l;
-    };
-
-    s = " " + s;
-    const sArr = s.split("");
+    // s = " " + s;
+    // const sArr = s.split("");
+    sArr.unshift(0);
     for (let i = 1; i <= len; i++) {
         for (let j = 0; j < countSettings; j++) {
-            h[j][i] = (h[j][i - 1] * xValue[j] + (sArr[i].charCodeAt(0) & 255)) % pMod[j];
+            h[j][i] = (h[j][i - 1] * xValue[j] + sArr[i]) % pMod[j];
+            r[j][i] = (r[j][i - 1] * xValue[j] + sArr[len - i + 1]) % pMod[j];
             x[j][i] = (x[j][i - 1] * xValue[j]) % pMod[j];
         }
     }
 
-    const zFunction = [0];
+    const zFunction = [len];
 
-    for (let i = 1; i < len; i++) {
-        zFunction.push(maxEqualString(i));
+    for (let i = 1; i < Math.ceil((len + 1) / 2); i++) {
+        if (isEqual(i, len - i, i)) {
+            zFunction.push(len - i);
+        }
     }
 
+    zFunction.sort((a, b) => a - b);
     return zFunction;
 }
 
@@ -100,9 +92,10 @@ _reader.on("line", (line) => {
 process.stdin.on("end", solve);
 
 function solve() {
-    const s = readString();
+    const n = readArray();
+    const s = readArray();
 
-    const result = lengthBasedString(s);
+    const result = mirroredString(n[0], n[1], s);
     console.log(result.join(" "));
 }
 
@@ -127,4 +120,4 @@ function readArray() {
     return arr;
 }
 
-module.exports = lengthBasedString;
+module.exports = mirroredString;
