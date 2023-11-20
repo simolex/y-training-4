25bb4a98539122c272d5f0c3cac30af5b3ff13e0
simolex <simolex@yandex.ru>
2023-11-20 16:43:59 +0300

    start task B

---
+++ Stage-4/task-B/index.test.js
@@ -0,0 +1,8 @@
+const solution = require(".");
+
+describe("B. Затерянный мир", () => {
+    test("test-1", () => {
+        const result = solution(8);
+        expect(result).toBe(92);
+    });
+});
