7327a0d5ec2fab066413f61946332689f15d3b90
simolex <simolex@yandex.ru>
2023-11-28 19:41:41 +0300

    add description for task A

--- Stage-Final/task-A/index.test.js
+++ Stage-Final/task-A/index.test.js
@@ -1,20 +1,16 @@
 const solution = require(".");
 
-describe("A. Partition", () => {
+describe("A. Объединение последовательностей", () => {
     test("test-1", () => {
-        const result = solution(5, [1, 3, 5, 5, 9], 3, [2, 5, 6]);
-        expect(result).toEqual([1, 2, 3, 5, 5, 5, 6, 9]);
+        const result = solution(1);
+        expect(result).toBe(1);
     });
     test("test-2", () => {
-        const result = solution(1, [0], 0, []);
-        // console.log(result.e);
-        // console.log(0 - result.e);
-        expect(result).toEqual([0]);
+        const result = solution(2);
+        expect(result).toBe(4);
     });
     test("test-3", () => {
-        const result = solution(0, [], 1, [0]);
-        // console.log(result.e);
-        // console.log(1 - result.e);
-        expect(result).toEqual([0]);
+        const result = solution(4);
+        expect(result).toEqual(9);
     });
 });
